73ab27f009447a8d637fcebe2ccdf7a09afba7b8
daniblou <danieltassa4@gmail.com>
2022-03-18 16:52:55 +0100

    Jai mis bootstrap et jquery et jai crée le fichier header

---
+++ front/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="asset/vendors/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    
+C'est ici que vous devez faire la barre de navigation
+<script src="asset/vendors/js/bootstrap.min.js"></script>
+</body>
+</html>  
