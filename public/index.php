ec517aaea37dd89fb5f673a1fbf8458c6440f414
daniblou <danieltassa4@gmail.com>
2022-03-21 18:21:22 +0100

    utilisation de la classe altorouter pour gérer le rotage

--- public/index.php
+++ public/index.php
@@ -1,13 +1,23 @@
 <?php 
 require '../vendor/autoload.php';
+$uri = $_SERVER['REQUEST_URI'];
+$router = new AltoRouter();
+$router->map('GET','/','home');
+$router->map('GET','/nous-contacter',function(){
+    echo 'contact';
+});
+$router->map('GET','/article/[*:slug]-[i:id]',function($slug , $id){
+    echo 'bon voyage';
+});
+$match = $router->match();
 
-$page = $_GET['page'] ?? '404.php';
-require '../front/header.php';
-    if($page === 'contact.php'){
-        require 'contact.php';
-    } else if($page === 404){
-        require 'error/404.php';
+if ($match != null){
+    if(is_callable($match['target'])){
+        call_user_func_array($match['target'], $match['params']); 
+        
     }
-    require '../front/footer.php';
+  
+}
 ?>
+
 
