bae2b22dfb08f626eac72326598b584fab34b501
Nandy NYONDA <nandynyonda3@gmail.com>
2022-03-21 11:29:28 +0100

    jai fais le header

--- front/header.php
+++ front/header.php
@@ -10,5 +10,24 @@
 <body>
     <p>header</p>
     
-
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link text-body font-weight-bolds active" aria-current="page" href="#">Home</a>
+        <a class="nav-link text-body" href="#">Features</a>
+        <a class="nav-link text-body position : absolute" href="#">Pricing</a>
+        <a class="nav-link text-body" href="#">Disabled</a>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+  <button class="btn btn-primary me-md-2" type="button">Button</button>
+  <button class="btn btn-primary" type="button">Button</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
  
