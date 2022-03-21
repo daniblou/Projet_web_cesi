a098909351185181aa94b7ddb8f45238d9820545
dlm <lydiana.djiogoua@viacesi.fr>
2022-03-21 15:12:28 +0100

    creation de la page de connexion

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php 
 require '../vendor/autoload.php';
 
-$page = $_GET['page'] ?? '404.php';
-require '../front/header.php';
-    if($page === 'contact.php'){
-        require 'contact.php';
+$page = $_GET['page'] ?? '404';
+//require '../front/header.php';
+    if($page === 'connexion'){
+        require 'connexion.php';
     } else if($page === 404){
         require 'error/404.php';
     }
-    require '../front/footer.php';
+  //  require '../front/footer.php';
 ?>
 
