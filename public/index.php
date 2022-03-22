86096ee3e908186eca1e6f05178b7cb1f41362a0
daniblou <danieltassa4@gmail.com>
2022-03-22 09:23:37 +0100

    Altorouter

--- public/index.php
+++ public/index.php
@@ -1,23 +1,4 @@
-<?php 
-require '../vendor/autoload.php';
-$uri = $_SERVER['REQUEST_URI'];
-$router = new AltoRouter();
-$router->map('GET','/','home');
-$router->map('GET','/nous-contacter',function(){
-    echo 'contact';
-});
-$router->map('GET','/article/[*:slug]-[i:id]',function($slug , $id){
-    echo 'bon voyage';
-});
-$match = $router->match();
 
-if ($match != null){
-    if(is_callable($match['target'])){
-        call_user_func_array($match['target'], $match['params']); 
-        
-    }
-  
-}
 ?>
 
 
